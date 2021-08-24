eb1cc4783999f88ae16cd0a285a0f5717afe49ce
Barnabas Asha <barnabee58@gmail.com>
2021-08-24 18:09:14 +0100

    Worked on the track orders page

--- src/Pages/Frontend/Interfaces/Track/Track.tsx
+++ src/Pages/Frontend/Interfaces/Track/Track.tsx
@@ -51,16 +51,16 @@ const Track:FC = () => {
             <h2 className="heading_2x">Track Item</h2>
             <div>
               <Search />
-              <div className="d-flex justify-content-between align-items-center mt-5">
+              <div className="d-flex justify-content-between align-items-start mt-5">
                 <div className="track-table shadow-sm">
                 <DataTable columns={columns} data={data} customStyles={customStyles} noHeader responsive />
                 </div>
                 <div className="track-info d-flex flex-column justify-content-center align-items-center shadow-sm p-4">
-                  <div className="shadow-sm my-4 mx-3 px-4 py-2">
+                  <div className="shadow-lg my-4 mx-3 px-4 py-2">
                     Your package is on the way
                   </div>
 
-                    <div className="track_details d-flex justify-content-start align-items-center">
+                  <div className="track_details d-flex justify-content-start align-items-center">
                         <div className="track_details-status w-50 p-3 d-flex justify-content-start align-items-center">
                         <i className="fas fa-box"></i>
                           <span>Ongoing trip</span>
@@ -69,7 +69,40 @@ const Track:FC = () => {
                           <span>Estimated time</span>
                           <time>1hr 20min</time>
                         </div>
+                  </div>
+
+                  <div className="track_delivery shadow-sm">
+                    <div className="track_delivery-top p-3">
+                     <div className="d-flex justify-content-between align-items-center">
+                        <span>15 September 2021</span>
+                        <div className="track_delivery-mode d-flex justify-content-between align-items-center p-2">
+                        <i className="fas fa-motorcycle"></i>
+                        Bike
+                        </div>
+                     </div>
+                     <div className="track_delivery-address d-flex justify-content-between align-items-center my-3">
+                        <span>
+                          Pickup<br/>Ikeja Lagos
+                        </span>
+                        <i className="fas fa-exchange-alt"></i>
+                        <span>
+                          Delivery<br/>Ikeja Lagos
+                        </span>
+                     </div>
+                     <hr className="bg-white p-1" />
+                     <div className="d-flex justify-content-between align-items-center my-3">
+                       <div>
+                         <small>sending</small><br />
+                         <span>Electronics and gadgets</span>
+                       </div>
+                       <div className="bg-info p-3"></div>
+                     </div>
+
+                     <div className="bg-white">
+
+                     </div>
                     </div>
+                  </div>
                 </div>
               </div>
             </div>
